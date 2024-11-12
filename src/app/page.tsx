358502a3d12a8cbe8ee5d5844310121d52dc1fd8
Sahil Saini <sahil.saini@celebaltech.com>
2024-11-12 10:42:21 +0000

    added categoreis to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import styles from "./page.module.css";
 import Navbar from "@/Components/Navbar/Navbar";
 import ArticleGrid from "@/Components/ArticleGrid/ArticleGrid";
+import CategoriesBar from "@/Components/CategoriesBar/CategoriesBar";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Navbar />
+      <CategoriesBar />
       <ArticleGrid />
     </div>
   );
