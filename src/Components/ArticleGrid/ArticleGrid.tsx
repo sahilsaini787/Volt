5cad520178a11cd888564d63f95d2366b44b7fdb
Sahil Saini <sahil.saini@celebaltech.com>
2024-11-12 08:37:13 +0000

    Implemented ArticleGrid

---
+++ src/Components/ArticleGrid/ArticleGrid.tsx
@@ -0,0 +1,21 @@
+import ArticleCard from "@/Components/ArticleCard/ArticleCard";
+import styles from "@/Components/ArticleGrid/ArticleGrid.module.scss";
+const ArticleGrid = ({}) => {
+  return (
+    <div className={styles.articleGridContainer}>
+      <ul className={styles.articleList}>
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+      </ul>
+    </div>
+  );
+};
+
+export default ArticleGrid;
