dd1d55156961a5d168699cd895970ad83bfc3d4b
Sahil Saini <sahil.saini@celebaltech.com>
2024-11-13 12:47:31 +0000

    Implemented Tag section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,49 @@
+import PopularTags from "@/Components/PopularTags/PopularTags";
 import styles from "./page.module.scss";
 import ArticleGrid from "@/Components/ArticleGrid/ArticleGrid";
 
+async function fetchTags() {
+  const apiURL = process.env.GRAPHQL_API_URL;
+  if (!apiURL) {
+    throw new Error("GRAPHQL_API_URL is not defined.");
+  }
+
+  try {
+    const response = await fetch(apiURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+          query getTags {
+            tags(where: {orderby: COUNT, order: DESC}, first: 10) {
+              nodes {
+                name
+                id
+                slug
+                count
+              }
+            }
+          }`,
+      }),
+      cache: "no-store",
+    });
+
+    const { data } = await response.json();
+    return data.tags.nodes;
+  } catch (error) {
+    console.log("FETCH ERROR: " + error);
+  }
+}
+
 export default async function Home() {
+  const tags = await fetchTags();
+
   return (
     <div className={styles.page}>
       <ArticleGrid />
+      <PopularTags tags={tags} />
     </div>
   );
 }
