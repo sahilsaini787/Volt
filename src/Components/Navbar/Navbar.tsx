c044e27a4dd2aaaed328f2266989b8b34b364f82
Sahil Saini <sahil.saini@celebaltech.com>
2024-11-14 04:42:29 +0000

    removed unwanted layout var

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -28,15 +28,14 @@ const Navbar = () => {
                 />
 
                 <g id="SVGRepo_iconCarrier">
-                  {" "}
                   <path
                     d="M2.5 0C1.11929 0 0 1.11929 0 2.5C0 3.70948 0.85888 4.71836 2 4.94999V9.5C2 11.433 3.567 13 5.5 13H7.29289L6.14645 14.1464L6.85355 14.8536L9.20711 12.5L6.85355 10.1464L6.14645 10.8536L7.29289 12H5.5C4.11929 12 3 10.8807 3 9.5V4.94999C4.14112 4.71836 5 3.70948 5 2.5C5 1.11929 3.88071 0 2.5 0Z"
                     fill="#1a2156"
-                  />{" "}
+                  />
                   <path
                     d="M8.85355 0.853554L8.14645 0.146446L5.79289 2.5L8.14645 4.85355L8.85355 4.14645L7.70711 3H9.5C10.8807 3 12 4.11929 12 5.5V10.05C10.8589 10.2816 10 11.2905 10 12.5C10 13.8807 11.1193 15 12.5 15C13.8807 15 15 13.8807 15 12.5C15 11.2905 14.1411 10.2816 13 10.05V5.5C13 3.567 11.433 2 9.5 2H7.70711L8.85355 0.853554Z"
                     fill="#1a2156"
-                  />{" "}
+                  />
                 </g>
               </svg>
             </div>
@@ -69,25 +68,23 @@ const Navbar = () => {
               className={styles.switchLayoutBtn}
               onClick={() => handleLayout()}
             >
-              {layout === "Grid" ? (
-                <svg
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="h-[1.2rem] w-[1.2rem] transition-all -rotate-90 scale-0"
-                >
-                  <rect width="7" height="7" x="3" y="3" rx="1"></rect>
-                  <rect width="7" height="7" x="3" y="14" rx="1"></rect>
-                  <path d="M14 4h7"></path>
-                  <path d="M14 9h7"></path>
-                  <path d="M14 15h7"></path>
-                  <path d="M14 20h7"></path>
-                </svg>
-              ) : (
-                <svg
+              <svg
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="h-[1.2rem] w-[1.2rem] transition-all -rotate-90 scale-0"
+              >
+                <rect width="7" height="7" x="3" y="3" rx="1"></rect>
+                <rect width="7" height="7" x="3" y="14" rx="1"></rect>
+                <path d="M14 4h7"></path>
+                <path d="M14 9h7"></path>
+                <path d="M14 15h7"></path>
+                <path d="M14 20h7"></path>
+              </svg>
+              {/* <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                   fill="none"
@@ -101,8 +98,7 @@ const Navbar = () => {
                   <rect width="7" height="7" x="14" y="3" rx="1"></rect>
                   <rect width="7" height="7" x="14" y="14" rx="1"></rect>
                   <rect width="7" height="7" x="3" y="14" rx="1"></rect>
-                </svg>
-              )}
+                </svg> */}
             </button>
           </div>
           <div className={styles.switchThemeBtnContainer}>
